572ee01d4289cb6fdd47ff1754f74947139d9039
martincodes <martin.schneider@techniknews.net>
2023-01-24 12:56:33 +0100

    rename project

---
+++ app/Console/Commands/TestDatabaseConnection.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Exception;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+
+class TestDatabaseConnection extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'system:test-db-connection';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'This command tests the database connection.';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle(): int
+    {
+        try {
+            DB::connection()->getPDO();
+            dump('Database is connected. Database Name is : ' . DB::connection()->getDatabaseName());
+        } catch (Exception) {
+            dump('Database connection failed');
+        }
+
+        return 0;
+    }
+}
