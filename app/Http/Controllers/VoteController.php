89cf587caf134c680fbdc9d1a6869321ce3af284
martincodes <martin.schneider@techniknews.net>
2023-01-30 12:45:43 +0100

    add controller to create votes

--- app/Http/Controllers/VoteController.php
+++ app/Http/Controllers/VoteController.php
@@ -2,13 +2,29 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Vote;
+use Exception;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
-use Illuminate\Http\Response;
 
 class VoteController extends Controller
 {
-    public function add(Request $request): Response
+    public function create(Request $request): JsonResponse
     {
+        $pollId = $request->input('poll_id');
+        $description = $request->input('description');
 
+        try {
+            $vote = new Vote();
+            $vote->poll_id = $pollId;
+            $vote->description = (int)$description;
+            $vote->save();
+        } catch (Exception $e) {
+            return \response()->json([
+                'error' => 'Creation failed.',
+            ], status: 500);
+        }
+
+        return \response()->json($vote, status: 201);
     }
 }
