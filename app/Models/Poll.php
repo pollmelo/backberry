378e6fa3776e38f02357b90b38835cf038a5f3b1
GrafBunsen <103031906+GrafBunsen@users.noreply.github.com>
2023-01-30 11:00:57 +0100

    Feature/16 create and set routing add method to pollcontroller (#24)
    * added a crate route to polls that creates a database entry with the specified entry in the request
    
    * Http Test working in real Database
    
    * HTTP Test now runs in in-memory DB
    
    * changed response to fit ticket
    
    * renamed test
    
    * ignored static code analysis
    
    * Update app/Http/Controllers/PollController.php
    
    Co-authored-by: martincodes <62392843+martincodes-de@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: martincodes <62392843+martincodes-de@users.noreply.github.com>

--- app/Models/Poll.php
+++ app/Models/Poll.php
@@ -2,10 +2,17 @@
 
 namespace App\Models;
 
+use DateTime;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
+/**
+ * @property string $name
+ * @property string $description
+ * @property DateTime $end_date
+ * @property int $phase
+ */
 class Poll extends Model
 {
     use HasFactory;
