8668994de25ef8ecbb5b62bbcbb93d0f9b6732e6
emsbe <emma-sophie.betz@web.de>
2023-01-31 16:31:17 +0000

    Add poll factory to create fake poll data mit FakePHP

---
+++ database/factories/PollFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Poll;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Poll>
+ */
+class PollFactory extends Factory
+{
+    protected $model = Poll::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'name' => fake()->realText(),
+            'description' => fake()->realText(),
+            'end_date' => fake()->date(),
+            'phase' => 1,
+        ];
+    }
+}
